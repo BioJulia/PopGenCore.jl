380016e2e68e9777f75041f809d5a0872b130e1c
Pavel Dimens <pdimens@live.com>
2021-07-29 09:56:11 -0400

    migrate types and IO from popgen.jl

--- src/PopGenIO.jl
+++ src/PopGenIO.jl
@@ -1,5 +1,17 @@
 module PopGenIO
 
 # Write your package code here.
+using CSV, DataFrames, NaturalSort, PooledArrays, Requires
+
+include("Datasets.jl")
+include("Delimited.jl")
+include("Genepop.jl")
+include("ioUtils.jl")
+include("Read.jl")
+include("Structure.jl")
+include("PopData.jl")
+include("VariantCall.jl")
+include("VariantCallGzLazy.jl")
+include("VariantCallLazy.jl")
 
 end
