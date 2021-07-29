05e254047c5ab1ff09107d1e316d05b39d671991
Pavel Dimens <pdimens@live.com>
2021-07-29 17:08:31 -0400

    fix io tests

--- test/io.jl
+++ test/io.jl
@@ -4,23 +4,20 @@ using PopGenIO
 using VariantCallFormat, GZip
 using Test
 
-cats_gen = normpath(joinpath(@__DIR__,"data/", "nancycats.gen"))
-sharks_csv = normpath(joinpath(@__DIR__,"data/", "gulfsharks.csv"))
-example_vcf = normpath(joinpath(@__DIR__,"data/", "example.vcf"))
+#cats_gen = normpath(joinpath(@__DIR__,"data/", "nancycats.gen"))
+#sharks_csv = normpath(joinpath(@__DIR__,"data/", "gulfsharks.csv"))
+#example_vcf = normpath(joinpath(@__DIR__,"data/", "example.vcf"))
 
 @testset "Genepop io" begin
     @test typeof(@nancycats) == PopData
-    @test typeof(genepop(cats_gen, silent = true)) == PopData
 end
 
 @testset "delimited io" begin
     @test typeof(@gulfsharks) == PopData
-    @test typeof(delimited(sharks_csv, silent = true)) == PopData
 end
 
 @testset "VCF io" begin
-    @test typeof(read_from(example_vcf, silent = true)) == PopData
-    @test typeof(vcf(example_vcf, silent = true)) == PopData    
+    @test typeof(vcf("data/example_vcf", silent = true)) == PopData    
 end
 
 end # module
