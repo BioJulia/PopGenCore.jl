5511548919e6a73c7cec04b2dd96fa88a9775633
Pavel Dimens <pdimens@live.com>
2021-07-29 16:09:10 -0400

    fix module

--- src/PopGenIO.jl
+++ src/PopGenIO.jl
@@ -12,10 +12,10 @@ include("VariantCall.jl")
 include("Read.jl")
 include("Datasets.jl")
 @init @require  VariantCallFormat="28eba6e3-a997-4ad9-87c6-d933b8bca6c1" begin
-    include("io/VariantCallLazy.jl")
+    include("VariantCallLazy.jl")
 end
 @init @require VariantCallFormat="28eba6e3-a997-4ad9-87c6-d933b8bca6c1" begin
-    @require GZip="92fee26a-97fe-5a0c-ad85-20a5f3185b63" include("io/VariantCallGzLazy.jl")
+    @require GZip="92fee26a-97fe-5a0c-ad85-20a5f3185b63" include("VariantCallGzLazy.jl")
 end
 
 end
