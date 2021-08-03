4893a7fa2e45e824f0a6f23132f1c2e30d805068
Pavel Dimens <pdimens@live.com>
2021-08-03 14:04:29 -0400

    notes and new show method

--- src/PopData2_alpha.jl
+++ src/PopData2_alpha.jl
@@ -13,38 +13,57 @@ x = @nancycats
 pd2pd2(x::PopData) = PopData2(x.meta, x.loci, x.loci.name.pool, x.loci.population.pool, x.loci.locus.pool)
 
 function Base.show(io::IO, data::PopData2)
-    println(io, "PopData Object")
     if occursin("Int16", string(eltype(data.genotypes.genotype)))
         marker = "Microsatellite"
     else
         marker = "SNP"
     end
-    n_loc = length(data.loci)
-    printstyled(io, n_loc, " ", marker, " loci", "\n" , bold = true)
     if "ploidy" ∈ names(data.metadata)
         ploidy = unique(data.metadata.ploidy) |> sort
         if length(ploidy) == 1
-            print(io, "  Ploidy: ") ; printstyled(io, ploidy |> join, "\n", bold = true)
+            ploidy = first(ploidy)
+            ploidytext = 
+                ploidy == 1 ? "Haploid" :
+                ploidy == 2 ? "Diploid" :
+                ploidy == 3 ? "Triploid" :
+                ploidy == 4 ? "Tetraploid" :
+                ploidy == 5 ? "Pentaploid" :
+                ploidy == 6 ? "Hexaploid" : "Octaploid"
         else
-            print(io, "  Ploidy (varies): ")
-            printstyled(io, ploidy[1], bold = true); [printstyled(io, ", $i", bold = true) for i in ploidy[2:end]]
-            print(io, "\n")
+            ploidytext = "Mixed-ploidy"
         end
     else
-        print(io, "  Ploidy:") ; printstyled(io, " absent\n", color = :yellow)
+        ploidytext = "Unknown-ploidy"
     end
+    n_loc = length(data.loci)
+    println(io, "PopData", "{" * ploidytext * ", ", n_loc, " " , marker * " loci}")
     print(io, "  Samples: ") ; printstyled(io, length(data.samples), "\n", bold = true)
     print(io, "  Populations: ") ; printstyled(io, length(data.populations), bold = true)
-
     if "longitude" ∈ names(data.metadata)
         miss_count = count(ismissing, data.metadata.longitude)
         if miss_count == length(data.metadata.longitude)
-            return
+            print("")
         elseif iszero(miss_count)
             print(io, "\n  Coordinates: present")
         else
             print(io, "\n  Coordinates: present (", count(ismissing, data.metadata.longitude), " missing)")
         end
     end
-    # add extracols section
+    allcols = vcat(names(data.metadata), names(data.genotypes)) |> unique
+    extracols = symdiff(allcols, ["name", "population", "ploidy", "longitude", "latitude", "locus", "genotype"])
+    if !isempty(extracols)
+        print(io, "\n  Other Info: ", extracols)
+    end
 end
+
+## Notes
+
+1. :name changing is bidirectional. Meaning you 
+can modify the name of a sample in .metadata and it will replace all occurences of it in .genotypes
+
+2. you can modify PopData.samples, PopData.loci, PopData.populations and it will apply all those
+changes to both dataframes
+
+3. adding a new population in .metadata will break the sync with the pools
+
+
