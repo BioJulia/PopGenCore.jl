22f4bd758a1a5f5dc91df17ff137cb483ba5d7a9
Pavel Dimens <pdimens@live.com>
2021-08-04 10:33:55 -0400

    rename fields and add internal constructor

--- src/PopData.jl
+++ src/PopData.jl
@@ -9,28 +9,34 @@ abstract type PopObj end
 """
 ```
 PopData
-    meta::DataFrame
-    loci::DataFrame
+    metadata::DataFrame
+    genotypes::DataFrame
 ```
 The data struct used for the PopGen population genetics ecosystem. You are
-**strongly** discouraged from manually creating tables to pass into a PopObj,
+**strongly** discouraged from manually creating tables to pass into a PopData,
 and instead should use the provided file importers.
 
-- `meta` ::DataFrame individual/sample data with the columns:
+- `metadata` ::DataFrame individual/sample data with the columns:
     - `name` ::String the individual/sample names
     - `population` ::String population names
     - `ploidy` ::Int8 ploidy in order of `ind`
     - `longitude` ::Float64 longitude values
     - `latitude` ::Float64 latitude values
-- `loci` ::DataFrame Long-format table of sample genotype records
+- `genotypes` ::DataFrame Long-format table of sample genotype records
     - name` ::PooledArray the individual/sample names
     - `population`::PooledArray of population names
     - `locus` ::PooledArray of locus names
     - `genotype` Tuple of Int8 or Int16 depending on SNP or microsatellite
 """
 struct PopData <: PopObj
-    meta::DataFrame
-    loci::DataFrame
+    metadata::DataFrame
+    genotypes::DataFrame
+    function PopData(meta::DataFrame, loci::DataFrame)
+        sort!(loci, [:locus, :population, :name], lt = natural)
+        sort(meta.name) != sort(loci.name.pool) && throw(ArgumentError("meta and loci dataframes do not contain the same sample names"))
+        sort(unique(meta.population)) != sort(loci.population.pool) && throw(ArgumentError("metadata and genotypes dataframes do not contain the same population names"))
+        new(meta, loci)
+    end
 end
 
 #=
@@ -61,44 +67,61 @@ cases.
 const GenoArray = AbstractVector{S} where S<:Union{Missing,Genotype}
 
 
-"""
-    Base.show(io::IO, data::PopData)
-Overloads `Base.show` for concise summary printing of a PopData object.
-"""
 function Base.show(io::IO, data::PopData)
-    println(io, "PopData Object")
-    if occursin("Int16", string(eltype(data.loci.genotype)))
+    if occursin("Int16", string(eltype(data.genotypes.genotype)))
         marker = "Microsatellite"
     else
         marker = "SNP"
     end
-    n_loc = length(unique(data.loci.locus))
-    printstyled(io, n_loc, " ", marker, " loci", "\n" , bold = true)
-    if "ploidy" ∈ names(data.meta)
-        ploidy = unique(data.meta.ploidy) |> sort
+    if "ploidy" ∈ names(data.metadata)
+        ploidy = unique(data.metadata.ploidy) |> sort
         if length(ploidy) == 1
-            print(io, "  Ploidy: ") ; printstyled(io, ploidy |> join, "\n", bold = true)
+            ploidy = first(ploidy)
+            ploidytext = 
+                ploidy == 1 ? "Haploid" :
+                ploidy == 2 ? "Diploid" :
+                ploidy == 3 ? "Triploid" :
+                ploidy == 4 ? "Tetraploid" :
+                ploidy == 5 ? "Pentaploid" :
+                ploidy == 6 ? "Hexaploid" : "Octaploid"
         else
-            print(io, "  Ploidy (varies): ")
-            printstyled(io, ploidy[1], bold = true); [printstyled(io, ", $i", bold = true) for i in ploidy[2:end]]
-            print(io, "\n")
+            ploidytext = "Mixed-ploidy"
         end
     else
-        print(io, "  Ploidy:") ; printstyled(io, " absent\n", color = :yellow)
+        ploidytext = "Unknown-ploidy"
     end
-    print(io, "  Samples: ") ; printstyled(io, length(data.loci.name.pool), "\n", bold = true)
-    print(io, "  Populations: ") ; printstyled(io, length(data.loci.population |> unique), "\n", bold = true)
-
-    if "longitude" ∈ names(data.meta)
-        if ismissing.(data.meta.longitude) |> all == true
-            print(io, "  Coordinates:") ; printstyled(io, " absent\n", color = :yellow)
-        elseif ismissing.(data.meta.longitude) |> all == false
-            println(io, "  Coordinates: present")
+    n_loc = length(data.genotypes.locus.pool)
+    println(io, "PopData", "{" * ploidytext * ", ", n_loc, " " , marker * " loci}")
+    println(io, "  Samples: $(length(data.metadata.name))") #; printstyled(io, length(data.samples), "\n", bold = true)
+    print(io, "  Populations: $(length(data.genotypes.population.pool))") # ; printstyled(io, length(data.populations), bold = true)
+    if "longitude" ∈ names(data.metadata)
+        miss_count = count(ismissing, data.metadata.longitude)
+        if miss_count == length(data.metadata.longitude)
+            print("")
+        elseif iszero(miss_count)
+            print(io, "\n  Coordinates: present")
         else
-            println(io, "  Coordinates: present (", count(i -> i === missing, data.meta.longitude), " missing)")
+            print(io, "\n  Coordinates: present (", count(ismissing, data.metadata.longitude), " missing)")
         end
-    else
-        print(io, "  Coordinates:") ; printstyled(io, " absent\n", color = :yellow)
     end
-    # add extracols section
+    allcols = vcat(names(data.metadata), names(data.genotypes)) |> unique
+    extracols = symdiff(allcols, ["name", "population", "ploidy", "longitude", "latitude", "locus", "genotype"])
+    if !isempty(extracols)
+        print(io, "\n  Other Info: ", extracols)
+    end
 end
+
+#=
+function metadata(data::PopData) 
+    s,f  = size(data.metadata)
+    dimtext = "(" * string(s) * " samples, " * string(f) * " fields)"
+    show(data.metadata, show_row_number = false, title = "Metadata of PopData " * dimtext )
+end
+
+function genotypes(data::PopData) 
+    l = length(data.genotypes.locus.pool)
+    s = length(data.metadata.name)
+    dimtext = "(" * string(s) * " samples, " * string(l) * " loci)"
+    show(data.genotypes, show_row_number = false, title = "Genotype information of PopData " * dimtext )
+end
+=#
