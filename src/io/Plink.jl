b525bf54ad3aef29479704eb4736b0ae5e902b44
Pavel Dimens <pdimens@live.com>
2021-09-20 15:29:12 -0400

    add plink .ped writer

--- src/io/Plink.jl
+++ src/io/Plink.jl
@@ -29,6 +29,26 @@ end
     Matrix{Union{Missing, NTuple{2,Int8}}}(_SNP.(genomatrix))
 end
 
+
+"""
+    plink(infile::String; keepfields::Symbol|Vector{Symbol}, silent::Bool)
+Read a PLINK `.ped` or binary `.bed` file into memory as a `PopData` object.
+Requires an accompanying `.fam` file in the same directory, but an accompanying `.bim` file is optional.
+- `infile::String` : path to `.ped` or `.bed` file
+
+### Keyword Arguments
+- `keepfields::Symbol|Vector{Symbol}` : which additional fields to import from the `.fam` file)
+    - `:all` [default]
+    - `:none`
+    - any one or combination of `[:sire, :dam, :sex, :phenotype]`
+- `silent::Bool`   : whether to print file information during import (default: `false`)
+
+## Example
+```julia
+para = plink("datadir/parakeet.ped", keepfields = :sex)
+parr = plink("datadir/parrot.bed", keepfields = [:sire, :dam])
+```
+"""
 function plink(infile::String; keepfields::Union{Symbol,Vector{Symbol}} = :all, silent::Bool = false)
     isfile(infile) || throw(ArgumentError("$infile not found."))
     basefile = splitext(infile)[1]
@@ -84,7 +104,7 @@ function plink(infile::String; keepfields::Union{Symbol,Vector{Symbol}} = :all, 
             :genotype => vec(_SNP(_plinkindex(reshape(data, (nrows, n))))) 
         )
     else
-        data = CSV.read(infile * ".ped", header = false, drop =1:6)
+        data = CSV.read(basefile * ".ped", DataFrame, header = false, drop =1:6)
         # TODO this is incomplete
     end
     metadf = generate_meta(genodf)
@@ -99,4 +119,59 @@ function plink(infile::String; keepfields::Union{Symbol,Vector{Symbol}} = :all, 
         end
     end
     return PopData(metadf, genodf)
+end
+
+### writing to PLINK format ###
+
+@inline function _genoconversion(genotype::T) where T<:Genotype
+    #=
+    00	Homozygous for first allele (0x00)
+    01	Missing genotype (0x01)
+    10	Heterozygous  (0x02)
+    11	Homozygous for second allele in .bim file (0x03)
+    =#
+    genotype == NTuple{2,Int8}((Int8(1),Int8(1))) ? 0x00 :
+        genotype == NTuple{2,Int8}((Int8(1),Int8(2))) ? 0x02 : 0x03
+end
+
+@inline function _genoconversion(genotype::Missing)
+    return 0x01
+end
+
+"""
+    plink(data::PopData; filename::String)
+
+Write a biallelic `PopData` object to PLINK `.ped` format with an accompanying
+`.fam` file. Genotypes are coded by the PLINK standard:
+- `00` Homozygous for first allele (`0x00`)
+- `01` Missing genotype (`0x01`)
+- `10` Heterozygous  (`0x02`)
+- `11` Homozygous for second allele (`0x03`)
+
+## Example
+```julia
+sharks = drop_multiallelic(@gulfsharks) ;
+plink(sharks, filename = "biallelic_sharks.ped")
+```
+"""
+function plink(data::PopData; filename::String)
+    data.info.biallelic != true && throw(ArgumentError("To write to PLINK format, data must be biallelic.\nThis can be done with drop_multiallelic(::PopData)\n"))
+    basename = endswith(filename, r".bed|.ped") ? splitext(filename)[1] : filename
+    # the .fam file
+    tmp = data.metadata[:, r"population|name|sire|dam|sex|phenotype"]
+    current_cols = propertynames(tmp)
+    fill_cols = symdiff(current_cols,[:population, :name, :sire, :dam, :sex, :phenotype])
+    for i in fill_cols
+        tmp[!, i] .= Int8(0)
+    end
+    select!(tmp, :population, :name, :sire, :dam, :sex, :phenotype)
+    CSV.write(basename * ".fam", tmp, delim = " ", writeheader = false, quotestrings = false)
+    # the .ped file
+    genomtx = loci_matrix(data)
+    converted_geno = mapreduce(hcat, eachcol(genomtx)) do col
+        _genoconversion.(col)
+    end
+    ped_df = hcat(tmp, DataFrame(converted_geno, :auto))
+    CSV.write(basename * ".ped", ped_df, delim = " ", writeheader = false, quotestrings = false)
+    return
 end
