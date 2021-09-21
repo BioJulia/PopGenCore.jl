2c7f90ccc5cba09bd61df50cba435d5ccc6a4428
Pavel Dimens <pdimens@live.com>
2021-09-21 11:39:50 -0400

    overhaul PopData.metadata

--- src/PopData.jl
+++ src/PopData.jl
@@ -12,7 +12,9 @@ abstract type PopObj end
 ```
 mutable struct PopDataInfo
     samples::Int64
+    sampeinfo::DataFrame
     loci::Int64
+    locusinfo::DataFrame
     populations::Int64
     ploidy::Union{Int8, Vector{Int8}}
     biallelic::Bool
@@ -23,32 +25,26 @@ about the `PopData` for easy access.
 """
 mutable struct PopDataInfo
     samples::Int64
+    sampleinfo::DataFrame
     loci::Int64
+    locusinfo::DataFrame
     populations::Int64
     ploidy::Union{Int8, Vector{Int8}}
     biallelic::Bool
 end
 
-# constructor for just the genodata dataframe
+# constructor FORMAT just the genodata dataframe
 function PopDataInfo(genodf::DataFrame)
-    ploidy = unique(DataFrames.combine(groupby(genodf, :name), :genotype => find_ploidy => :ploidy))
-    ploidy = length(ploidy) == 1 ? ploidy[1] : ploidy
+    sampleinfo = unique(dropmissing(genodf), :name)
+    insertcols!(sampleinfo, :ploidy => length.(sampleinfo.genotype))
+    select!(sampleinfo, :name, :population, :ploidy)
+    ploidy = unique(sampleinfo.ploidy)
+    ploidy = length(ploidy) == 1 ? Int8(ploidy[1]) : Int8.(ploidy)
     PopDataInfo(
         length(genodf.name.pool),
+        sampleinfo,
         length(genodf.locus.pool),
-        length(genodf.population.pool),
-        ploidy,
-        isbiallelic(genodf)
-    )
-end
-
-
-function PopDataInfo(metadf::DataFrame, genodf::DataFrame)
-    ploidy = unique(metadf.ploidy)
-    ploidy = length(ploidy) == 1 ? ploidy[1] : ploidy
-    PopDataInfo(
-        length(metadf.name),
-        length(genodf.locus.pool),
+        DataFrame(:chromosome => Int8(0), :locus => genodf.locus.pool, :cm => Int8(0), :bp => Int8(0)),
         length(genodf.population.pool),
         ploidy,
         isbiallelic(genodf)
@@ -56,79 +52,77 @@ function PopDataInfo(metadf::DataFrame, genodf::DataFrame)
 end
 
 function Base.show(io::IO, data::PopDataInfo)
-    println(io, "    samples: ", data.samples)
-    println(io, "       loci: ", data.loci)
-    println(io, "populations: ", data.populations)
-    println(io, "     ploidy: ", data.ploidy...)
-    println(io, "  biallelic: ", data.biallelic)
+    println(io, " ploidy:        ", data.ploidy...)
+    println(io, " # loci:        ", data.loci)
+    println(io, " # samples:     ", data.samples)
+    println(io, " # populations: ", data.populations)
+    println(io, " biallelic:     ", data.biallelic)
 end
 
 """
 ```
 PopData
-    metadata::DataFrame
+    metadata::PopDataInfo
     genodata::DataFrame
-    info::PopDataInfo
 ```
 The data struct used for the PopGen population genetics ecosystem. You are
 **strongly** discouraged from manually creating tables to pass into a PopData,
 and instead should use the provided file importers and utilities.
 
-- `metadata` DataFrame of sample information with the columns:
-    - `name` - sample names [`String`]
-    - `population` - population names [`String`]
-    - `ploidy` - sample ploidy [`Int8`]
-    - `longitude` - longitude values [`Float32`, optional]
-    - `latitude` - latitude values   [`Float32`, optional]
+- `metadata` PopDataInfo of  data information
+    - `samples` - the number of samples in the data
+    - `sampleinfo` - DataFrame of sample names,populations, ploidy, etc.
+    - `loci` - the number of loci in the data
+    - `locusinfo` - DataFrame of locus names, chromosome, physical position, etc.
+    - `populations` - the number of populations in the data
+    - `ploidy` - the ploidy (or ploidies) present in the data
+    - `biallelic` - if all the markers are biallelic
 - `genodata` DataFrame of sample genotype records
     - `name` - the individual/sample names [`PooledArray`]
     - `population` - population names [`PooledArray`]
     - `locus` - locus names [`PooledArray`]
     - `genotype` - genotype values [`NTuple{N,Signed}`]
-- `info` PopDataInfo of summary data information
-    - `samples` - the number of samples in the data
-    - `loci` - the number of loci in the data
-    - `populations` - the number of populations in the data
-    - `ploidy` - the ploidy (or ploidies) present in the data
-    - `biallelic` - if all the markers are biallelic
+
 """
 struct PopData <: PopObj
-    metadata::DataFrame
+    metadata::PopDataInfo
     genodata::DataFrame
-    info::PopDataInfo
-    function PopData(meta::DataFrame, loci::DataFrame)
-        if "ploidy" ∉ names(meta)
-            insertcols!(meta, 3, :ploidy => Int8(0))
+    function PopData(meta::PopDataInfo, loci::DataFrame)
+        if "ploidy" ∉ names(meta.sampleinfo)
+            insertcols!(meta.sampleinfo, 3, :ploidy => Int8(0))
         end
         metacols = ["name", "population", "ploidy"]
-        metacheck = intersect(metacols, names(meta))
+        metacheck = intersect(metacols, names(meta.sampleinfo))
         if metacheck != metacols
-            throw(error("metadata missing columns $(symdiff(metacheck, metacols))"))
+            throw(error("metadata sampleinfo missing columns $(symdiff(metacheck, metacols))"))
         end
         genocols = ["name", "population", "locus", "genotype"]
         genocheck = intersect(genocols, names(loci))
         if genocheck != genocols
             throw(error("genodata missing columns $(symdiff(genocheck, genocols))"))
         end
-        sort(meta.name) != sort(loci.name.pool) && throw(ArgumentError("meta and loci dataframes do not contain the same sample names"))
-        sort(unique(meta.population)) != sort(loci.population.pool) && throw(ArgumentError("metadata and genotypes dataframes do not contain the same population names"))
-        new(meta, loci, PopDataInfo(meta,loci))
+        sort(meta.sampleinfo.name) != sort(loci.name.pool) && throw(ArgumentError("metadata and genodata do not contain the same sample names"))
+        sort(unique(meta.sampleinfo.population)) != sort(loci.population.pool) && throw(ArgumentError("metadata and genodata do not contain the same population names"))
+        new(PopDataInfo(loci), loci)
     end
 end
 
+PopData(data::DataFrame) = PopData(PopDataInfo(data), data)
 
 function PopDataInfo!(data::PopData)
-    data.info.samples = length(data.genodata.name.pool)
-    data.info.loci = length(data.genodata.locus.pool)
-    data.info.populations = length(data.genodata.population.pool)
-    if "ploidy" ∈ names(data.metadata)
-        ploidy = unique(data.metadata.ploidy)
+    data.metadata.samples = length(data.genodata.name.pool)
+    data.metadata.loci = length(data.genodata.locus.pool)
+    data.metadata.populations = length(data.genodata.population.pool)
+    filter!(:name => x -> x .== data.genodata.name.pool,  data.metadata.sampleinfo)
+    filter!(:locus => x -> x .== data.genodata.locus.pool,  data.metadata.locusinfo)
+    if "ploidy" ∈ names(data.metadata.sampleinfo)
+        ploidy = unique(data.metadata.sampleinfo.ploidy)
         ploidy = length(ploidy) == 1 ? ploidy[1] : ploidy
     else
         ploidy = Int8(0)
     end       
-    data.info.ploidy = ploidy
-    data.info.biallelic = data.info.biallelic ? true : isbiallelic(data)
+    data.metadata.ploidy = ploidy
+    data.metadata.biallelic = data.metadata.biallelic ? true : isbiallelic(data)
     return
 end
 
@@ -163,6 +157,7 @@ The definition as an `AbstractVector` adds flexibility for `SubArray`
 cases.
 """
 const GenoArray = AbstractVector{S} where S<:Union{Missing,Genotype}
+#const SnpArray = PooledVector{Union{Missing, Tuple{Int8, Int8}}, UInt8, Vector{UInt8}} 
 
 function _ploidy2text(ploidy::Int8)
     ploidy == 0 ? "" :
@@ -182,24 +177,22 @@ function Base.show(io::IO, data::PopData)
     else
         marker = "SNP"
     end
-    ploidy = _ploidy2text(data.info.ploidy)
+    ploidy = _ploidy2text(data.metadata.ploidy)
     ploidy = ploidy == "" ? "" : ploidy * ", "
-    n_loc = data.info.loci
+    n_loc = data.metadata.loci
     println(io, "PopData", "{" * ploidy, n_loc, " " , marker * " loci}")
-    println(io, "  Samples: $(data.info.samples)")
-    print(io, "  Populations: $(data.info.populations)")
-    if "longitude" ∈ names(data.metadata)
-        miss_count = count(ismissing, data.metadata.longitude)
-        if miss_count == length(data.metadata.longitude)
+    println(io, "  Samples: $(data.metadata.samples)")
+    print(io, "  Populations: $(data.metadata.populations)")
+    if "longitude" ∈ names(data.metadata.sampleinfo)
+        miss_count = count(ismissing, data.metadata.sampleinfo.longitude)
+        if miss_count == length(data.metadata.sampleinfo.longitude)
             print("")
-        elseif iszero(miss_count)
-            print(io, "\n  Coordinates: present")
         else
-            print(io, "\n  Coordinates: present (", count(ismissing, data.metadata.longitude), " missing)")
+            print(io, "\n  Coordinates: present")
         end
     end
-    allcols = vcat(names(data.metadata), names(data.genodata)) |> unique
-    extracols = symdiff(allcols, ["name", "population", "ploidy", "longitude", "latitude", "locus", "genotype"])
+    allcols = vcat(names(data.metadata.sampleinfo), names(data.genodata)) |> unique
+    extracols = symdiff(allcols, ["name", "population", "ploidy", "locus", "genotype"])
     if !isempty(extracols)
         print(io, "\n  Other Info: ", extracols)
     end
@@ -207,13 +200,13 @@ end
 
 
 """
-    metadata(::PopData)
+    sampleinfo(::PopData)
 Method to show the `PopData` `metadata` field. 
 """
-function metadata(data::PopData)
-    s,f  = size(data.metadata)
+function sampleinfo(data::PopData)
+    s,f  = size(data.metadata.sampleinfo)
     dimtext = "(" * string(s) * " samples, " * string(f) * " fields)"
-    show(data.metadata, show_row_number = false, title = "Metadata of PopData " * dimtext )
+    show(data.metadata.sampleinfo, show_row_number = false, title = "Sample Information of PopData " * dimtext )
 end
 
 """
@@ -222,27 +215,32 @@ Method to show the `PopData` `genodata` field.
 """
 function genodata(data::PopData) 
     l = length(data.genodata.locus.pool)
-    s = length(data.metadata.name)
+    s = length(data.metadata.sampleinfo.name)
     dimtext = "(" * string(s) * " samples, " * string(l) * " loci)"
     show(data.genodata, show_row_number = false, title = "Genotype information of PopData " * dimtext )
 end
 
+function locusinfo(data::PopData)
+    s,f  = size(data.metadata.locusinfo)
+    dimtext = "(" * string(s) * " loci, " * string(f) * " fields)"
+    show(data.metadata.locusinfo, show_row_number = false, title = "Locus Information of PopData " * dimtext )
+end
 
 function Base.getindex(data::PopData, idx::Symbol)
-    if idx == :metadata
-        return data.metadata
+    if idx == :sampleinfo
+        return data.metadata.sampleinfo
     elseif idx == :genodata
         return data.genodata
     elseif idx == :name
-        return data.metadata.name
+        return data.metadata..sampleinfo.name
     elseif idx == :population
-        return data.metadata.population
+        return data.metadata.sampleinfo.population
     elseif idx == :ploidy
-        return data.metadata.ploidy
+        return data.metadata.sampleinfo.ploidy
     elseif idx == :locus
         return data.genodata.locus.pool
     elseif idx == :coordinates
-        return data.metadata[:, [:longitude, :latitude]]
+        return data.metadata.sampleinfo[:, [:longitude, :latitude]]
     else
         throw(ArgumentError("Cannot directly index PopData with the \':$idx\' field"))
     end
@@ -257,6 +255,6 @@ function Base.getindex(data::PopData, args...)
         3 => (i -> PooledArray(i, compress = true)) => :locus,
         4
     )
-    newmeta = intersect(data.metadata.name, geno.name.pool) != data.metadata.name ? data.metadata[data.metadata.name .∈ Ref(geno.name.pool), :] : data.metadata
-    PopData(newmeta, geno)
+    #newmeta = intersect(data.metadata.sampleinfo.name, geno.name.pool) != data.metadata.sampleinfo.name ? data.metadata.sampleinfo[data.metadata.sampleinfo.name .∈ Ref(geno.name.pool), :] : data.metadata.sampleinfo
+    PopData(geno)
 end
