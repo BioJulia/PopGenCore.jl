718ebb8c2430f6bee441af9ee6796df80c44e1bc
Pavel Dimens <pdimens@live.com>
2021-10-27 13:48:50 -0400

    rm nightly from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
 quiet = length(ARGS) > 0 && ARGS[1] == "-q"
-anyerrors = false
+anyerrors = 0
 
 using PopGenCore
 using Test
@@ -9,31 +9,31 @@ all_tests = [
     "popdata.jl",
     "types.jl",
     "allelegenofreq.jl",
-     "io.jl",
-     "manipulate.jl",
-     "conditionals.jl",
-     "utils.jl",
-     "iterators.jl"
+    "io.jl",
+    "manipulate.jl",
+    "utils.jl",
+    "conditionals.jl",
+    "iterators.jl"
 ]
 
 println("Running tests:")
 
-for a_test in all_tests
-    try
-        include(a_test)
-        println("\t\033[1m\033[32mPASSED\033[0m: $(a_test)")
-    catch e
-        global anyerrors = true
-        println("\t\033[1m\033[31mFAILED\033[0m: $(a_test)")
-        if fatalerrors
-            rethrow(e)
-        elseif !quiet
-            showerror(stdout, e, backtrace())
-            println()
+@testset "All Tests" begin
+    for a_test in all_tests
+        try
+            include(a_test)
+            println("\t\033[1m\033[32mPASSED\033[0m: $(a_test)")
+        catch e
+            global anyerrors += 1
+            println("\t\033[1m\033[31mFAILED\033[0m: $(a_test)")
+            if fatalerrors
+                rethrow(e)
+            elseif !quiet
+                showerror(stdout, e, backtrace())
+                println()
+            end
         end
     end
 end
 
-if anyerrors
-    throw("Oh no, some tests failed!  :(")
-end
+anyerrors > 0 && throw("$anyerrors files of tests failed :(")
