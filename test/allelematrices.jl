012d0c0eedae27851f9e828a9b340cd20f39e9a4
Pavel Dimens <pdimens@live.com>
2022-05-20 16:36:15 -0400

    remove PopGen. prefix

--- test/allelematrices.jl
+++ test/allelematrices.jl
@@ -7,17 +7,17 @@ cats = @nancycats ;
 
 @testset "AlleleMatrices.jl" begin
     @testset "allelematrix" begin
-        @test size(PopGen.allelematrix(cats, by = "count", missings = "mean", scale = false, center = false)) == (237, 108)
-        @test size(PopGen.allelematrix(cats, by = "frequency", missings = "missing", scale = false, center = false)) == (237, 108)
-        @test size(PopGen.allelematrix(cats, by = "frequency", missings = "zero", scale = false, center = false)) == (237, 108)
-        @test size(PopGen.allelematrix(cats, by = "frequency", missings = "mean", scale = false, center = false)) == (237, 108)
-        @test size(PopGen.allelematrix(cats, scale = true)) == (237, 108)
-        @test size(PopGen.allelematrix(cats, center = true)) == (237, 108)
-        @test PopGen.allelematrix(cats, scale = true) != PopGen.allelematrix(cats, center = true)
-        a = PopGen.allelematrix(cats, by = "count")
-        b = PopGen.allelematrix(cats, missings = "missing")
-        c = PopGen.allelematrix(cats, missings = "zero")
-        d = PopGen.allelematrix(cats)
+        @test size(allelematrix(cats, by = "count", missings = "mean", scale = false, center = false)) == (237, 108)
+        @test size(allelematrix(cats, by = "frequency", missings = "missing", scale = false, center = false)) == (237, 108)
+        @test size(allelematrix(cats, by = "frequency", missings = "zero", scale = false, center = false)) == (237, 108)
+        @test size(allelematrix(cats, by = "frequency", missings = "mean", scale = false, center = false)) == (237, 108)
+        @test size(allelematrix(cats, scale = true)) == (237, 108)
+        @test size(allelematrix(cats, center = true)) == (237, 108)
+        @test allelematrix(cats, scale = true) != PopGen.allelematrix(cats, center = true)
+        a = allelematrix(cats, by = "count")
+        b = allelematrix(cats, missings = "missing")
+        c = allelematrix(cats, missings = "zero")
+        d = allelematrix(cats)
         @test all(a .== b) == false
         @test all(a .== c) == false
         @test all(a .== d) == false
